cf82b9a8f4a0c16dba2c9efb3f51766bfd7a6243
cloudazure1985 <91127547+cloudazure1985@users.noreply.github.com>
2021-09-22 11:12:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
