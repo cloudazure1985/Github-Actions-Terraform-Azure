36c3d55c2eb642676c3d4b5581f84b347f4fb3f8
cloudazure1985 <91127547+cloudazure1985@users.noreply.github.com>
2021-09-22 11:06:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,3 @@ terraform {
     key                  = "dev.terraform.tfstate"
   }
 }
-module "RG" {
-  source   = "./modules/RG" #A
-  rgname   = var.rgname     #B
-  location = var.location
-}
-module "SA" {
-  source   = "./modules/StorageAccount"
-  sname    = var.sname
-  rgname   = var.rgname
-  location = var.location
-}
